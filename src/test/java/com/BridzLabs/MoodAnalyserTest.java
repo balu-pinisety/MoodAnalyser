b350df666968d8966961cdd13cb0833e0cfb468f
balu-pinisety <manikantapandu225@gmail.com>
2021-07-10 21:12:09 +0530

    Refactor: Test code to add message in constructor

--- src/test/java/com/BridzLabs/MoodAnalyserTest.java
+++ src/test/java/com/BridzLabs/MoodAnalyserTest.java
@@ -13,16 +13,16 @@ public class MoodAnalyserTest {
 	//Test case for Sad Mood
 	@Test
 	public void givenMessgae_WhenSad_ShouldReturn_Sad() {
-		MoodAnalyser moodAnalyser = new MoodAnalyser(); //passed message to constructor
-		String mood = moodAnalyser.analyseMood("I am in Sad Mood");
+		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood"); //passed message to constructor
+		String mood = moodAnalyser.analyseMood();
 		Assert.assertEquals("SAD", mood);
 	}
 	
 	//Test case for Sad Mood
 	@Test
 	public void givenMessage_whenNotSad_shouldReturn_Happy() {
-		MoodAnalyser moodAnalyser = new MoodAnalyser(); //passed message to constructor
-		String mood = moodAnalyser.analyseMood("I am in any Mood");
+		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any Mood"); //passed message to constructor
+		String mood = moodAnalyser.analyseMood();
 		Assert.assertEquals("HAPPY", mood);
 	}
 	
