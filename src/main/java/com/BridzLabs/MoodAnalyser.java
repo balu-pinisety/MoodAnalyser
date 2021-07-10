18442e8a6559a47b04f1baa9fafe78e043485899
balu-pinisety <manikantapandu225@gmail.com>
2021-07-10 20:00:46 +0530

    Added: Method of analyseMood function which returns Happy or Sad

--- src/main/java/com/BridzLabs/MoodAnalyser.java
+++ src/main/java/com/BridzLabs/MoodAnalyser.java
@@ -1,9 +1,21 @@
 package com.BridzLabs;
 
 /**
- * @author BALU
- * Program to Handle custom Exceptions by Mood Analyzer Problem 
+ * @author BALU Program to Handle custom Exceptions by Mood Analyzer Problem
  */
 public class MoodAnalyser {
-
+	
+	/**
+	 * Ability to check the given string in the given message
+	 * @param message
+	 * @return HAPPY or SAD
+	 */
+	public String analyseMood(String message) {
+		if (message.contains("Sad")) {
+			return "SAD";
+		} else {
+			return "HAPPY";
+		}
+	}
+	
 }
